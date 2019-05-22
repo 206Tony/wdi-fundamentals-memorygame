a99d54a8708bb0543c4b5746959e55cab6310158
Anthony Zardis <anthonyzardis@gmail.com>
2019-05-22 11:35:02 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+
+console.log("User picked " + cardThree);
+console.log("User picked " + cardFour);
+
