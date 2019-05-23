43496122833256628910edbf5450e35a38435755
Anthony Zardis <anthonyzardis@gmail.com>
2019-05-23 13:30:27 -0700

    added more game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,23 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
 
-console.log("User picked " + cardThree);
-console.log("User picked " + cardFour);
+if (cardsInPlay.length === 2) {
+	console.log(true);
+  if (cardsInPlay[0] !== cardsInPlay[1]) {
+		alert("Sorry, try again.");
+	} else {
+ 		alert("You found a match!");
+	}
+}
+
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+
+
+
+
 
