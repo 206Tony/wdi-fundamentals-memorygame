3e0cf64e4d00e04093651ea82af143e727ce0281
Anthony Zardis <anthonyzardis@gmail.com>
2019-05-23 16:01:51 -0700

    added functionality to memory game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,29 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
 
-if (cardsInPlay.length === 2) {
-	console.log(true);
-  if (cardsInPlay[0] !== cardsInPlay[1]) {
-		alert("Sorry, try again.");
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
 	} else {
- 		alert("You found a match!");
+		console.log("Sorry, try again.");
 	}
 }
 
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
+function flipCard(cardId) {
+	cardsInPlay.push(cards[cardId]); 
+	console.log("User flipped " + cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		
+	  checkForMatch();
+	 }
+}
+
+flipCard(0);
+flipCard(2);
+
+
+
+
 
 
 
